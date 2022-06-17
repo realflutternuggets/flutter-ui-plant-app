8bf1b0843c795bb666eb3d2f55d43e1cb3aeaa76
Thomas Uhuru <102694446+realflutternuggets@users.noreply.github.com>
2022-06-17 13:10:32 +0300

    Added Profile Page
    Added Profile Page

--- lib/ui/screens/profile_page.dart
+++ lib/ui/screens/profile_page.dart
@@ -1,19 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/constants.dart';
+import 'package:flutter_onboarding/widgets/profile_widget.dart';
 
-class ProfilePage extends StatefulWidget {
+class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
-}
-
-class _ProfilePageState extends State<ProfilePage> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Profile Page'),
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        height: size.height,
+        width: size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              child: const CircleAvatar(
+                radius: 60,
+                backgroundImage: ExactAssetImage('assets/images/profile.jpg'),
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Constants.primaryColor.withOpacity(.5),
+                  width: 5.0,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: size.width * .3,
+              child: Row(
+                children: [
+                  Text(
+                    'John Doe',
+                    style: TextStyle(
+                      color: Constants.blackColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                      height: 24,
+                      child: Image.asset("assets/images/verified.png")),
+                ],
+              ),
+            ),
+            Text(
+              'johndoe@gmail.com',
+              style: TextStyle(
+                color: Constants.blackColor.withOpacity(.3),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              height: size.height * .7,
+              width: size.width,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                  ProfileWidget(
+                    icon: Icons.person,
+                    title: 'My Profile',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.settings,
+                    title: 'Settings',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.notifications,
+                    title: 'Notifications',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.chat,
+                    title: 'FAQs',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.share,
+                    title: 'Share',
+                  ),
+                  ProfileWidget(
+                    icon: Icons.logout,
+                    title: 'Log Out',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
---
+++ lib/ui/screens/widgets/profile_widget.dart
@@ -0,0 +1,50 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_onboarding/constants.dart';
+
+class ProfileWidget extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  const ProfileWidget({
+    Key? key,
+    required this.icon,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                color: Constants.blackColor.withOpacity(.5),
+                size: 24,
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                  color: Constants.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Icon(
+            Icons.arrow_forward_ios,
+            color: Constants.blackColor.withOpacity(.4),
+            size: 16,
+          )
+        ],
+      ),
+    );
+  }
+}
